c4ade54759020714dffafe2a072c9413a3e737b3
Linh Nguyen <linhn@mit.edu>
2021-10-28 23:09:14 -0400

    Add 3 templates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { Template } from './Template';
-
+import './template.css';
+import { RainbowTemplate, GradientTemplate, BlankTemplate } from './Template';
 import { LEDSquare } from './LEDSquare';
 
 function App() {
@@ -9,12 +9,19 @@ function App() {
       <header className="App-header">
         <h1>Design Your Light Fibers</h1>
         <h2>Choose a Template</h2>
-        <Template />
+        <div className="template-row">
+          <RainbowTemplate/>
+          <GradientTemplate/>
+        </div>
+        <div className="template-row">
+          <BlankTemplate/>
+        </div>
 
         <h2>Waistband Pattern</h2>
         <p>Click on a square to customize your LED strip!</p>
         <p>Here is an example square for you to click on :) </p>
         <LEDSquare onChange={() => {}}/>
+
         <h2>Effects (TBD)</h2>
       </header>
     </div>
--- src/Template.tsx
+++ src/Template.tsx
@@ -1,13 +1,63 @@
 import './template.css';
 
-const rainbowColors = ["#FF0000", "#FF8A00", "#FFF500"];
+const rainbowColors = ["#FF0000", "#FF8A00", "#FFF500", "#9EE05C", "#1ED700", "#65E5D6", "#2097DB", "#3242D0", "#B240CF", "#D64EA8"];
+const gradientColors = ["#BE0505", "#D40000", "#FF0000", "#FF4040", "#FF6D6D", "#FF8B8B", "#FFB0B0", "#FAC9C9", "#FFDDDD", "#FFEBEB"];
+const blankColors = ["#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", ]
 
-export const Template = () => {
+// TODO: Fix malfunctioning string matching here to remove hardcoded templates later
+function getColorStrip(templateName: string): string[] {
+    // templateName = "rainbow";
+    // console.log(""+templateName);
+    switch (templateName) {
+        case "rainbow":
+            return rainbowColors;
+        case "blank":
+            return blankColors;
+        default:
+            return [];
+            // throw Error("Bad input: Template does not exist");
+    }
+        
+} 
+
+export const RainbowTemplate = (templateName: string) => {
+    const colorStrip = getColorStrip("blank");
     return (
-        <div>
-            {rainbowColors.map(color => {
-                return <div className="box" style={{backgroundColor: color}}/>
-            })}
+        <div className="template">
+            <div className="template-color-strip">
+                {rainbowColors.map(color => {
+                    return <div className="template-box" style={{backgroundColor: color}}/>
+                })}
+            </div>
+            <p className="template-label">Rainbow</p>
+        </div>
+    )
+}
+
+export const GradientTemplate = (templateName: string) => {
+    const colorStrip = getColorStrip("blank");
+    return (
+        <div className="template">
+            <div className="template-color-strip">
+                {gradientColors.map(color => {
+                    return <div className="template-box" style={{backgroundColor: color}}/>
+                })}
+            </div>
+            <p className="template-label">Gradient</p>
+        </div>
+    )
+}
+
+export const BlankTemplate = (templateName: string) => {
+    const colorStrip = getColorStrip("blank");
+    return (
+        <div className="template">
+            <div className="template-color-strip">
+                {blankColors.map(color => {
+                    return <div className="template-box" style={{backgroundColor: color}}/>
+                })}
+            </div>
+            <p className="template-label">Blank</p>
         </div>
     )
 }
