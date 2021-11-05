5bc2a460f11e0958901012598eb9dbde879c4bfd
Linh Nguyen <linhn@mit.edu>
2021-11-05 13:11:03 -0400

    install axios and cors

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import './template.css';
 import { Template } from './Template';
 import { PatternSelect } from './PatternSelect';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 // TODO: Make constants file
 const rainbowColors = ["#FF0000", "#FF8A00", "#FFF500", "#9EE05C", "#1ED700", "#65E5D6", "#2097DB", "#3242D0", "#B240CF", "#D64EA8"];
 const gradientColors = ["#BE0505", "#D40000", "#FF0000", "#FF4040", "#FF6D6D", "#FF8B8B", "#FFB0B0", "#FAC9C9", "#FFDDDD", "#FFEBEB"];
 const blankColors = ["#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", "#C4C4C4", ]
 
-const handleSubmit = () => {
-//   const userObject = {
-//     name: this.state.name,
-//     email: this.state.email
-// };
+const handleSubmit = (ledColors) => {
+  const data = {
+    ledColors: ledColors,
+};
 
-  // axios.post('http://localhost:5000/update_design', userObject)
+  // axios.post('http://localhost:5000/update_design', data)
   //     .then((res) => {
   //         console.log(res.data)
   //     }).catch((error) => {
   //         console.log(error)
   //     });
-
-  // this.setState({ name: '', email: '' })
+  axios({
+        url: 'http://localhost:5000/update_design',
+        method: 'put',
+        data: data,
+    });
 };
 
 function App() {
