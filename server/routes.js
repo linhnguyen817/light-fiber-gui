26ea59b3e94139eeb3510c48fec5c2e1fa06d3e3
Jennifer Wang <jennwang@mit.edu>
2021-12-02 14:34:31 -0500

    please im begging u

--- server/routes.js
+++ server/routes.js
@@ -37,7 +37,7 @@ recordRoutes.route('/update_design').post(function(req, res) {
 });
 
 recordRoutes.route('*').get(function (req, res){
-    res.sendFile(path.join(__dirname+'/src/build/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build'));
   });
 
 
